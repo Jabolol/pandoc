fb6d81f6d93a1bf9c6b8017d8baddabf927d0543
Javier R <javier.rios-urbano@epitech.eu>
2024-04-10 01:14:18 +0200

    add: functionality to test `xml` and `json` parsers

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,19 @@
 module Main (main) where
 
-import Lib
+import qualified Core as C
+import qualified System.Environment as E
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- E.getArgs
+  case args of
+    [path, "json"] -> handleConversion path C.parseJSON C.jToString "JSON"
+    [path, "xml"] -> handleConversion path C.parseXML C.xToString "XML"
+    _ -> putStrLn "Usage: ./pandoc /path/to/file type <json/xml>"
+
+handleConversion :: FilePath -> (String -> Maybe a) -> (a -> String) -> String -> IO ()
+handleConversion path parseFn toStringFn formatName = do
+  fileContents <- readFile path
+  case parseFn fileContents of
+    Just value -> putStrLn (toStringFn value)
+    Nothing -> putStrLn $ "Failed to parse " ++ formatName
