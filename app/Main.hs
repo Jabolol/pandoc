423671dcbe512af6148eeab084166f593ec31421
Javier R <javier.rios-urbano@epitech.eu>
2024-04-11 13:28:58 +0200

    fix: print parsed `json` document

--- app/Main.hs
+++ app/Main.hs
@@ -1,35 +1,17 @@
 module Main (main) where
 
 import qualified Core as C
-import qualified System.Environment as E
+import qualified Text.Pretty.Simple as P
 
 main :: IO ()
 main = do
-  args <- E.getArgs
-  case args of
-    [path, "json"] ->
-      handleConversion
-        path
-        C.parseJSON
-        C.jToString
-        "JSON"
-    [path, "xml"] ->
-      handleConversion
-        path
-        C.parseXML
-        C.xToString
-        "XML"
-    [path, "md"] ->
-      handleConversion
-        path
-        C.parseMarkdown
-        C.mToString
-        "Markdown"
-    _ -> putStrLn "Usage: ./pandoc /path/to/file type"
-
-handleConversion :: FilePath -> (String -> Maybe a) -> (a -> String) -> String -> IO ()
-handleConversion path parseFn toStringFn formatName = do
-  fileContents <- readFile path
-  case parseFn fileContents of
-    Just value -> putStrLn (toStringFn value)
-    Nothing -> putStrLn $ "Failed to parse " ++ formatName
+  contents <- readFile "examples/syntaxe.json"
+  let parsed = C.parseJSON contents
+  case parsed of
+    Just value -> do
+      let sth = C.jsonToDocument value
+      case sth of
+        Left err -> putStrLn err
+        Right doc -> P.pPrint doc
+    _ -> error "Failed to parse JSON"
+  return ()
