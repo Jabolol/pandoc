5ff95f9448b3c21706d55f0face348e0f8c26949
Javier R <javier.rios-urbano@epitech.eu>
2024-04-11 21:49:49 +0200

    add: test `xml` files

--- app/Main.hs
+++ app/Main.hs
@@ -5,13 +5,13 @@ import qualified Text.Pretty.Simple as P
 
 main :: IO ()
 main = do
-  contents <- readFile "examples/syntaxe.json"
-  let parsed = C.parseJSON contents
+  contents <- readFile "examples/syntaxe.xml"
+  let parsed = C.parseXML contents
   case parsed of
     Just value -> do
-      let sth = C.jsonToDocument value
+      let sth = C.xmlToDocument value
       case sth of
         Left err -> putStrLn err
         Right doc -> P.pPrint doc
-    _ -> error "Failed to parse JSON"
+    _ -> error "Failed to parse XML"
   return ()
