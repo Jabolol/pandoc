36b7d5ad533e88f30dac61b42467dd60b834a081
Javier R <javier.rios-urbano@epitech.eu>
2024-04-13 21:09:32 +0200

    add: `I/O` demo

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,22 @@
 module Main (main) where
 
 import qualified Core as C
-import qualified Text.Pretty.Simple as P
+import qualified System.Environment as E
 
 main :: IO ()
 main = do
-  contents <- readFile "examples/syntaxe.xml"
-  let parsed = C.parseXML contents
-  case parsed of
-    Just value -> do
-      let sth = C.xmlToDocument value
-      case sth of
-        Left err -> putStrLn err
-        Right doc -> P.pPrint doc
-    _ -> error "Failed to parse XML"
-  return ()
+  args <- E.getArgs
+  case args of
+    [inputPath, outputPath] -> do
+      contents <- readFile inputPath
+      let parsed = C.parseXML contents
+      case parsed of
+        Just value -> do
+          let sth = C.xmlToDocument value
+          case sth of
+            Left err -> putStrLn err
+            Right doc -> do
+              writeFile outputPath $ C.jToString $ C.documentToJson doc
+              putStrLn $ "Conversion successful. Output written to " ++ outputPath
+        _ -> error "Failed to parse XML"
+    _ -> error "Usage: programName inputPath outputPath"
